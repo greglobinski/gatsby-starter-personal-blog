e810bfc138480ac22283feb01ca8d42587e513af
greglobinski <greglobinski@gmail.com>
2018-03-02 11:25:13 +0100

    window !== undefined added to isWideScreen helper

--- src/utils/helpers.js
+++ src/utils/helpers.js
@@ -1,8 +1,10 @@
 import theme from "../styles/theme";
 
 export function isWideScreen() {
-  const windowWidth = window.innerWidth;
-  const mediaQueryL = theme.mediaQueryTresholds.L;
+  if (typeof window !== `undefined`) {
+    const windowWidth = window.innerWidth;
+    const mediaQueryL = theme.mediaQueryTresholds.L;
 
-  return windowWidth >= mediaQueryL;
+    return windowWidth >= mediaQueryL;
+  }
 }
