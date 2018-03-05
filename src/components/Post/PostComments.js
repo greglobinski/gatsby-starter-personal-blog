35b52e4d0b1201c00302e213ff8db6d68f84f228
greglobinski <greglobinski@gmail.com>
2018-03-05 16:52:53 +0100

    fb 2

--- src/components/Post/PostComments.js
+++ src/components/Post/PostComments.js
@@ -9,29 +9,44 @@ const styles = theme => ({
 
 class PostComments extends React.Component {
   componentDidMount() {
+    this.loadFbLoginApi();
+
     if (typeof FB !== "undefined") {
       //eslint-disable-next-line no-undef
       FB.XFBML.parse();
     }
   }
+
+  loadFbLoginApi() {
+    window.fbAsyncInit = function() {
+      FB.init({
+        appId: 340409213030244,
+        cookie: true,
+        xfbml: true,
+        version: "v2.5"
+      });
+    };
+
+    console.log("Loading fb api");
+    // Load the SDK asynchronously
+    (function(d, s, id) {
+      var js,
+        fjs = d.getElementsByTagName(s)[0];
+      if (d.getElementById(id)) return;
+      js = d.createElement(s);
+      js.id = id;
+      js.src = "//connect.facebook.net/en_US/sdk.js";
+      fjs.parentNode.insertBefore(js, fjs);
+    })(document, "script", "facebook-jssdk");
+  }
+
   render() {
     const { classes, post, slug } = this.props;
 
     return (
       <div>
         <h1>{slug}</h1>
-        <script
-          key={`fb-setup`}
-          dangerouslySetInnerHTML={{
-            __html: `(function(d, s, id) {
-      var js, fjs = d.getElementsByTagName(s)[0];
-      if (d.getElementById(id)) return;
-      js = d.createElement(s); js.id = id;
-      js.src = 'https://connect.facebook.net/en_US/sdk.js#xfbml=1&version=v2.12&appId=340409213030244&autoLogAppEvents=1';
-      fjs.parentNode.insertBefore(js, fjs);
-    }(document, 'script', 'facebook-jssdk'));`
-          }}
-        />
+
         <div id="fb-root" />
         <span
           className="fb-comments-count"
@@ -56,6 +71,21 @@ PostComments.propTypes = {
 };
 
 export default injectSheet(styles)(PostComments);
+
+/*
+<script
+key={`fb-setup`}
+dangerouslySetInnerHTML={{
+  __html: `(function(d, s, id) {
+var js, fjs = d.getElementsByTagName(s)[0];
+if (d.getElementById(id)) return;
+js = d.createElement(s); js.id = id;
+js.src = 'https://connect.facebook.net/en_US/sdk.js#xfbml=1&version=v2.12&appId=340409213030244&autoLogAppEvents=1';
+fjs.parentNode.insertBefore(js, fjs);
+}(document, 'script', 'facebook-jssdk'));`
+}}
+/>
+*/
 
 // <script
 //           key={`fb-setup`}
