5d27db6cf7fe264e9cfe414afeb1b0094e665335
greglobinski <greglobinski@gmail.com>
2018-03-08 23:52:12 +0100

    Remove wrong and unnecessary config for gatsby-plugin-feed

--- gatsby-config.js
+++ gatsby-config.js
@@ -165,56 +165,7 @@ module.exports = {
       }
     },
     {
-      resolve: `gatsby-plugin-feed`,
-      options: {
-        query: `
-          {
-            site {
-              siteMetadata {
-                title
-                description
-                siteUrl
-                site_url: siteUrl
-              }
-            }
-          }
-        `,
-        feeds: [
-          {
-            serialize: ({ query: { site, allMarkdownRemark } }) => {
-              return allMarkdownRemark.edges.map(edge => {
-                return Object.assign({}, edge.node.frontmatter, {
-                  description: edge.node.excerpt,
-                  url: site.siteMetadata.siteUrl + edge.node.frontmatter.path,
-                  guid: site.siteMetadata.siteUrl + edge.node.frontmatter.path,
-                  custom_elements: [{ "content:encoded": edge.node.html }]
-                });
-              });
-            },
-            query: `
-              {
-                allMarkdownRemark(
-                  limit: 1000,
-                  sort: { fields: [frontmatter___date], order: DESC }
-                ) {
-                  edges {
-                    node {
-                      excerpt
-                      html
-                      frontmatter {
-                        title
-                        date
-                        path
-                      }
-                    }
-                  }
-                }
-              }
-            `,
-            output: "/rss.xml"
-          }
-        ]
-      }
+      resolve: `gatsby-plugin-feed`
     },
     {
       resolve: `gatsby-plugin-sitemap`
