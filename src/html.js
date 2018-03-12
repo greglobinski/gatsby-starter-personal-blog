5f4e84474593f5fe6f90687aa4807b09670af4c4
greglobinski <greglobinski@gmail.com>
2018-03-12 13:32:03 +0100

    Add CSS rendering to html.js

--- src/html.js
+++ src/html.js
@@ -1,8 +1,21 @@
 /* eslint  react/prop-types: 0 */
 import React from "react";
 
+let stylesStr;
+if (process.env.NODE_ENV === `production`) {
+  try {
+    stylesStr = require(`!raw-loader!../public/styles.css`);
+  } catch (e) {
+    console.log(e);
+  }
+}
+
 module.exports = class HTML extends React.Component {
   render() {
+    let css;
+    if (process.env.NODE_ENV === `production`) {
+      css = <style id="gatsby-inlined-css" dangerouslySetInnerHTML={{ __html: stylesStr }} />;
+    }
     return (
       <html {...this.props.htmlAttributes}>
         <head>
@@ -10,6 +23,7 @@ module.exports = class HTML extends React.Component {
           <meta httpEquiv="x-ua-compatible" content="ie=edge" />
           <meta name="viewport" content="width=device-width, initial-scale=1" />
           {this.props.headComponents}
+          {css}
           <meta name="apple-mobile-web-app-capable" content="yes" />
           <meta name="apple-mobile-web-app-status-bar-style" content="#D0E0D8" />
           <meta name="apple-mobile-web-app-title" content="Lazywill" />
