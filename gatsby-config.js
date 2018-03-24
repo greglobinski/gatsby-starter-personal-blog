6bf371171f610947fd12151399a26c0da1388b79
greglobinski <greglobinski@gmail.com>
2018-03-24 14:06:28 +0100

    Fix bug in gatsby-remark-images setup

--- gatsby-config.js
+++ gatsby-config.js
@@ -85,7 +85,8 @@ module.exports = {
           {
             resolve: `gatsby-remark-images`,
             options: {
-              maxWidth: 800
+              maxWidth: 800,
+              backgroundColor: "transparent"
             }
           },
           {
