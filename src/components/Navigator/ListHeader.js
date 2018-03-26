bd5930c10597a4d0936debe11aad26b2245df6c3
ZuoLan <i@zuolan.me>
2018-03-26 20:52:29 +0800

    Fix a filter display logic problem
    Remove a judgment condition, mask the filter with zIndex, and redraw the borderTop line.

--- src/components/Navigator/ListHeader.js
+++ src/components/Navigator/ListHeader.js
@@ -9,6 +9,17 @@ import ExpandLessIcon from "material-ui-icons/ExpandLess";
 const styles = theme => ({
   closed: {
     display: "none",
+    zIndex: 99,
+    background: theme.base.colors.background,
+    "&::after": {
+      content: `""`,
+      position: "absolute",
+      top: 0,
+      left: theme.base.sizes.linesMargin,
+      right: theme.base.sizes.linesMargin,
+      height: 0,
+      borderTop: `1px solid ${theme.base.colors.lines}`
+    },
     ".is-aside.closed &, .moving-featured.closed &": {
       display: "flex",
       flexDirection: "row",
@@ -95,8 +106,7 @@ const ListHeader = props => {
           </IconButton>
         </div>
       )}
-      {navigatorShape === "open" &&
-        categoryFilter !== "all posts" && (
+      {categoryFilter !== "all posts" && (
           <div className={classes.filter}>
             <small>Active category filter:</small> <strong>{categoryFilter}</strong>
             <IconButton
