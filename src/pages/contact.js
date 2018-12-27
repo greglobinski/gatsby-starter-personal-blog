baf7dd88ecf21008019c1872d10cf1daf3458d0c
Hendra <khairendra@users.noreply.github.com>
2018-12-28 04:00:57 +0700

    Update contact.js

--- src/pages/contact.js
+++ src/pages/contact.js
@@ -1,8 +1,9 @@
 import React from "react";
 import PropTypes from "prop-types";
-import injectSheet from "react-jss";
 import Obfuscate from "react-obfuscate";
 
+import withRoot from "../withRoot";
+import Layout from "../components/Layout";
 import Main from "../components/Main";
 import Article from "../components/Main/Article";
 import PageHeader from "../components/Page/PageHeader";
@@ -10,20 +11,20 @@ import Content from "../components/Main/Content";
 import Form from "../components/ContactForm";
 import config from "../../content/meta/config";
 
-const styles = theme => ({});
-
 const Contact = () => {
   return (
-    <Main>
-      <Article>
-        <PageHeader title="Contact" />
-        <Content>
-          Feel free to contact me by email: <Obfuscate email={config.contactEmail} /> or use the
-          form below.
-        </Content>
-        <Form />
-      </Article>
-    </Main>
+    <Layout>
+      <Main>
+        <Article>
+          <PageHeader title="Contact" />
+          <Content>
+            Feel free to contact me by email: <Obfuscate email={config.contactEmail} /> or use the
+            form below.
+          </Content>
+          <Form />
+        </Article>
+      </Main>
+    </Layout>
   );
 };
 
@@ -31,4 +32,4 @@ Contact.propTypes = {
   classes: PropTypes.object.isRequired
 };
 
-export default injectSheet(styles)(Contact);
+export default withRoot(Contact);
